1cccb4fe676a53f58449c8e7f20f55f7fd10c628
Berenice Medel <bemesa21@gmail.com>
2025-03-10 18:26:09 -0600

    show cities list, toggle heart icon

--- lib/climora_web/live/home_live.ex
+++ lib/climora_web/live/home_live.ex
@@ -37,19 +37,66 @@ defmodule ClimoraWeb.HomeLive do
         </div>
       </div>
     </.simple_form>
+    <div phx-update="stream" id="city_list">
+      <div
+        :for={{id, city} <- @streams.resulting_cities}
+        class="bg-white top-100 z-40 w-full "
+        id={id}
+      >
+        <div class="cursor-pointer w-full border-gray-100 rounded-t border-b">
+          <div class="flex w-full items-center p-2 pl-2 border-transparent border-l-2 relative hover:border-teal-100">
+            <div class="w-full items-center flex">
+              <div class="mx-2 -mt-1 w-full ">
+                {city.name}
+                <div class="text-xs truncate w-full normal-case font-normal -mt-1 text-gray-500">
+                  {city.country}
+                </div>
+              </div>
+              <button
+                id={"#{id}_solid"}
+                type="button"
+                class="w-10 flex-none hidden"
+                phx-click={JS.show(to: "##{id}_no_solid") |> JS.hide()}
+              >
+                <.icon
+                  name="hero-heart-solid"
+                  class="w-7 h-7  bg-red-400 border-red hover:bg-red-400"
+                />
+              </button>
+              <button
+                id={"#{id}_no_solid"}
+                type="button"
+                class="w-10 flex-none"
+                phx-click={
+                  JS.show(to: "##{id}_solid") |> JS.hide() |> JS.push("set_favorite", value: city)
+                }
+              >
+                <.icon name="hero-heart" class="w-7 h-7 bg-red-400 hover:bg-red-400" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     """
   end
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :hola, :hye)
+    socket =
+      socket
+      |> assign(:cities_search, to_form(%{"city_name" => ""}, as: "cities_search"))
+      |> stream(:resulting_cities, [])
 
-    form = to_form(%{"city_name" => ""}, as: "cities_search")
-    {:ok, assign(socket, :cities_search, form)}
+    {:ok, socket}
   end
 
   def handle_event("search_city", %{"cities_search" => %{"city_name" => city}}, socket) do
-    get_city_coordinates(city)
+    {_status, cities} = get_city_coordinates(city)
+    {:noreply, stream(socket, :resulting_cities, cities)}
+  end
 
+  def handle_event("set_favorite", params, socket) do
+    IO.inspect("todo, create_favorite")
     {:noreply, socket}
   end
 
@@ -57,14 +104,30 @@ defmodule ClimoraWeb.HomeLive do
     url = "#{@api_url}?q=#{URI.encode(city)}&limit=2&appid=#{@api_key}"
 
     with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <- HTTPoison.get(url) do
-      response = Jason.decode!(body) |> IO.inspect()
-      {:ok, response}
+      {:ok, get_cities_info(body)}
     else
       {:ok, %HTTPoison.Response{status_code: status_code}} ->
         IO.inspect("Failed to get a city #{status_code}")
+        {:error, []}
 
       {:error, reason} ->
         IO.inspect("Failed to get a city #{reason}")
+        {:error, []}
     end
+  end
+
+  defp get_cities_info(body) do
+    body
+    |> Jason.decode!()
+    |> Enum.with_index()
+    |> Enum.map(fn {city, idx} ->
+      %{
+        id: "city_#{idx}",
+        country: city["country"],
+        lat: city["lat"],
+        lon: city["lon"],
+        name: city["name"]
+      }
+    end)
   end
 end
